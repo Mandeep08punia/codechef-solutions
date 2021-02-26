23accc33da0ada61418fbe147ed4a767cd669870
Mandeep Punia <48849295+Mandeep08punia@users.noreply.github.com>
2021-02-26 22:12:42 +0530

    Chef and Laddus For Children
    Chef and Laddus For Children

---
+++ CCBTS03.cpp
@@ -0,0 +1,90 @@
+/*Author:mp04*/
+//Link:https://www.codechef.com/CCHI2021/problems/CCBTS03
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+#define pb(i) push_back(i)
+#define pp() pop_back()
+#define f first
+#define s second
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rrep(i, n) for (int i = ((n)-1); i >= 0; i--)
+#define mk(l, r) make_pair(l, r)
+#define all(vc) vc.begin(), vc.end()
+#define gc getchar_unlocked
+#define ms(i, j) memset(i, j, sizeof i)
+#define w(t) while (t--)
+#define precise(x) fixed << setprecision(x)
+const ll MAX = 1e5 + 5;
+const ll INF = 2e18;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+//using namespace std;
+using namespace __gnu_pbds;
+typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update> PBDS;
+
+#define FASTIO                      \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(NULL);                    \
+  cout.tie(NULL)
+
+void sieve(int n)
+{
+  vector<int> v;
+  bitset<1000000> b;
+  b.set();
+  b[0] = b[1] = 0;
+
+  for (int i = 2; i <= n; i++)
+  {
+    if (b[i] == 1)
+    {
+      v.pb(i);
+      for (int j = i * i; j <= n; j += i)
+      {
+        b[j] = 0;
+      }
+    }
+  }
+  for (int i = 0; i < v.size(); i++)
+  {
+    cout << v[i] << " ";
+  }
+  cout << endl;
+}
+
+void solve()
+{
+  int n, k;
+  cin >> n >> k;
+  int a[n], mini, maxi, diff;
+  for (int i = 0; i < n; i++)
+    cin >> a[i];
+  sort(a, a + n);
+  mini = a[0];
+  maxi = a[k - 1];
+  diff = maxi - mini;
+  for (int i = k; i < n; i++) {
+    maxi = a[i];
+    mini = a[i - k + 1];
+    diff = min(diff, maxi - mini);
+  }
+  cout << diff << '\n';
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+#endif
+  FASTIO;
+  int t  ;
+  cin >> t;
+  while (t--)
+  {
+    solve();
+  }
+  return 0;
+}
